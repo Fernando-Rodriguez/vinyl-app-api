7d0f7394475d44966d98ec6ccb69af16b462b4bc
Fernando-Rodriguez <62259661+Fernando-Rodriguez@users.noreply.github.com>
2020-10-21 19:39:55 -0400

    fgb
    gdfg

--- VinylAppApi.Library/DbModels/OwnedAlbumModelDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VinylAppApi.Library.DbModels
-{
-    public class OwnedAlbumModelDto
-    {
-        public string User { get; set; }
-        public string Album { get; set; }
-        public string Artist { get; set; }
-    }
-}
