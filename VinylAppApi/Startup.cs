ebe99dc819595ddca8b2442588fd4911f1b9a8a3
Fernando-Rodriguez <62259661+Fernando-Rodriguez@users.noreply.github.com>
2021-05-23 21:46:35 -0400

    small changes

--- VinylAppApi/Startup.cs
+++ VinylAppApi/Startup.cs
@@ -102,7 +102,6 @@ namespace VinylAppApi
             services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
